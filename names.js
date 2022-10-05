4c4a12a5bdbca733bdd65fba937e3f6220daa66f
swaroopRajwal <swaroop.rajwal18@gmail.com>
2022-10-05 10:14:57 +0530

    feat: rendering cards from an array

---
+++ names.js
@@ -0,0 +1,51 @@
+const names = [{
+  name: "Raja Harshvardhan",
+  fullName: "Raja Harsh Vardhan Singh",
+  description: "Full stack developer app developer and open source enthusiast",
+  githubUrl: "https://github.com/rhvsingh",
+  image: "https://avatars.githubusercontent.com/u/70860738?v=4",
+}, {
+  name: "Shivam Singh",
+  fullName: "Shivam Singh",
+  description: "Frontend web developer and open source enthusiast",
+  githubUrl: "https://github.com/shiv-03",
+  image: "https://avatars.githubusercontent.com/u/89941409?v=4",
+}, {
+  name: "Keval vavaliya",
+  fullName: "Keval vavaliya",
+  description: "Full stack developer app developer and open source enthusiast",
+  githubUrl: "https://github.com/kevalvavaliya",
+  image: "https://avatars.githubusercontent.com/u/72963356?v=4",
+}, {
+  name: "Rishabh Pratap Singh",
+  fullName: "Rishabh Pratap Singh",
+  description: "Devops and open source enthusiast",
+  githubUrl: "https://github.com/iam-Rps",
+  image: "https://avatars.githubusercontent.com/u/96458739?v=4",
+}, {
+  name: "harikanani",
+  fullName: "Harikanani",
+  description: "Backend Developer | Python | Node.js",
+  githubUrl: "https://github.com/harikanani",
+  image: "https://avatars.githubusercontent.com/u/112641910?v=4https://avatars.githubusercontent.com/u/112641910?v=4",
+}, {
+  name: "Christiano Ronaldo",
+  fullName: "Keval vavaliya",
+  description: "World class football player",
+  githubUrl: "#",
+  image: "https://avatars.githubusercontent.com/u/97455464?s=400&u=fd1d04ca1220415fb675005bc2eba59896609912&v=4",
+}, {
+  name: "Nitesh Thapliyal",
+  fullName: "Nitesh Thapliyal",
+  description: "Tech Enthusiast",
+  githubUrl: "https://github.com/Nitesh-thapliyal",
+  image: "https://avatars.githubusercontent.com/u/53345517?v=4"
+}, {
+  name: "Swaroop Rajwal",
+  fullName: "Swaroop Jaikumar Rajwal",
+  description: "Tech_nooB",
+  githubUrl: "https://github.com/swaroopRajwal",
+  image: "https://i.imgur.com/89kI44N.jpg"
+}]
+
+export default names;
---
+++ script.js
@@ -0,0 +1,48 @@
+import names from "./names.js";
+
+const mainContainer = document.querySelector("#main-container");
+
+names.forEach(item => {
+  const card = document.createElement("div");
+  card.classList.add("card1");
+  const contentBox = `
+    <div class="contentBx">
+        <div class="content">
+            <h1>${item.name}</h1>
+        </div>
+        <div class="card mx-auto" style="width: 18rem">
+            <img src=${item.image}
+                class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${item.fullName}</h5>
+                <p class="card-text" style="line-height: 2">
+                  ${item.description}
+                </p>
+                <a href=${item.githubUrl} class="btn btn-primary">Github</a>
+            </div>
+        </div>
+    </div>
+    <div class="arrow-button"><i class="fa fa-angle-down"></i></div>
+  `
+  card.innerHTML = contentBox;
+  mainContainer.appendChild(card);
+})
+
+const card = document.querySelectorAll(".card1");
+
+const arrowButton = document.querySelectorAll(".arrow-button");
+
+for (let index = 0; index < arrowButton.length; index++) {
+  // console.log(card[index].children[0].scrollHeight);
+  arrowButton[index].onclick = (e) => {
+      // console.log(card[index].clientHeight);
+      card[index].classList.toggle("active");
+      // console.log(card[index].style.height);
+      // card[index].style.height = `${card[index].children[0].scrollHeight}px`
+      if (card[index].classList.contains("active")) {
+          card[index].style.height = `${card[index].children[0].scrollHeight}px`
+      } else {
+          card[index].style.height = '400px'
+      }
+  };
+}
